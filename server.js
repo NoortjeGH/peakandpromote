5b5288ff4fad1159051ed5b6824014fc64cd059d
NoortjeGH <peakandpromote@gmail.com>
2026-06-16 15:56:52 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.post('/api/analyze', async (req, res) => {
   const { naam, bedrijfsnaam, branche, probleem } = req.body;
